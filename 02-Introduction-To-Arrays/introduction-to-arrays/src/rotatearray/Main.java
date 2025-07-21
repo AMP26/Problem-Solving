76109e491a00681c68457704400d0ec5ba516e9e
AMP26 <amit.patnaik0126@gmail.com>
2025-07-21 20:16:26 +0530

    Roatate Array

--- 02-Introduction-To-Arrays/introduction-to-arrays/src/rotatearray/Main.java
+++ 02-Introduction-To-Arrays/introduction-to-arrays/src/rotatearray/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
         int k = 3;
+//      int k = 9;
 
         Solution sol = new Solution();
         sol.rotateArray(nums, k);
