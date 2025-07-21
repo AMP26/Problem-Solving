cf387d652b6b8917076e8fd6a01e020414488879
AMP26 <amit.patnaik0126@gmail.com>
2025-07-21 18:04:57 +0530

    Maximun & Minimun in an Array

--- 02-Introduction-To-Arrays/introduction-to-arrays/src/minmax/Main.java
+++ 02-Introduction-To-Arrays/introduction-to-arrays/src/minmax/Main.java
@@ -1,4 +1,10 @@
 package minmax;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 6, 0, -1, 10, 90};
+
+        Solution sol = new Solution();
+        sol.minMax(arr);
+    }
 }
--- 02-Introduction-To-Arrays/introduction-to-arrays/src/minmax/Solution.java
+++ 02-Introduction-To-Arrays/introduction-to-arrays/src/minmax/Solution.java
@@ -1,4 +1,14 @@
 package minmax;
 
 public class Solution {
+
+    public void minMax(int[] arr) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+
+        for(int n : arr) {
+            if(min > n) { min = n; }
+            if(max < n) { max = n; }
+        }
+        System.out.println("Maximum: " + max + " & " + "Minimum: " + min);
+    }
 }
