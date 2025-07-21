eb84d05a240443dc51c3904c37a232faedcd9b23
AMP26 <amit.patnaik0126@gmail.com>
2025-07-21 18:37:04 +0530

    Separate Odd & Even Numbers in an Array

--- 02-Introduction-To-Arrays/introduction-to-arrays/src/oddeven/Main.java
+++ 02-Introduction-To-Arrays/introduction-to-arrays/src/oddeven/Main.java
@@ -2,6 +2,9 @@ package oddeven;
 
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {2, 9, 7, 5, 4, 6, 1, 8};
 
+        Solution sol = new Solution();
+        sol.printOddEven(arr);
     }
 }
--- 02-Introduction-To-Arrays/introduction-to-arrays/src/oddeven/Solution.java
+++ 02-Introduction-To-Arrays/introduction-to-arrays/src/oddeven/Solution.java
@@ -3,14 +3,17 @@ package oddeven;
 public class Solution {
 
     public void printOddEven(int[] arr) {
+        System.out.println("Even Numbers: ");
         for(int n : arr) {
-            System.out.println("Even Numbers: ");
             if(n % 2 == 0) { System.out.print(n + " "); }
         }
 
+        System.out.println("\nOdd Numbers: ");
         for(int n : arr) {
-            System.out.println("Odd Numbers: ");
             if(n % 2 != 0) { System.out.print(n + " "); }
         }
     }
 }
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
