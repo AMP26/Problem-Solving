e14f0e15dadb088cef1cfec30dcfd7a757ae4ca4
AMP26 <amit.patnaik0126@gmail.com>
2025-07-23 01:53:31 +0530

    Special Index

--- 03-Prefix-Sum/prefix-sum/src/specialindex/Main.java
+++ 03-Prefix-Sum/prefix-sum/src/specialindex/Main.java
@@ -1,7 +1,16 @@
 package specialindex;
 
+
 public class Main {
     public static void main(String[] args) {
 
+//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        int[] arr = {2, 1, 6, 4};
+          int[] arr = {1, 1, 1};
+
+        Solution sol = new Solution();
+        int count = sol.specialIndex(arr);
+
+        System.out.println("Number of Special Index: " + count);
     }
 }
--- 03-Prefix-Sum/prefix-sum/src/specialindex/Solution.java
+++ 03-Prefix-Sum/prefix-sum/src/specialindex/Solution.java
@@ -2,6 +2,36 @@ package specialindex;
 
 public class Solution {
     public int specialIndex(int[] arr) {
-        return -1;
+        int[] oddPS = new int[arr.length];
+        int[] evenPS = new int[arr.length];
+
+        // Prefix Sum:  Odd & Even
+        evenPS[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                evenPS[i] = evenPS[i - 1] + arr[i];
+                oddPS[i] = oddPS[i - 1];
+            } else {
+                oddPS[i] = oddPS[i - 1] + arr[i];
+                evenPS[i] = evenPS[i - 1];
+            }
+        }
+
+        int sumO = 0, sumE = 0, n = arr.length - 1, count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                sumO = evenPS[n] - oddPS[i];
+                sumE = oddPS[n] - evenPS[i];
+            } else {
+                sumO = oddPS[i - 1] + (evenPS[n] - evenPS[i]);
+                sumE = evenPS[i - 1] + (oddPS[n] - oddPS[i]);
+            }
+            count += (sumO == sumE) ? 1 : 0;
+        }
+
+        return count;
     }
 }
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
