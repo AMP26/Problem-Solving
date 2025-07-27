de58ac2e51bca0ff76d8e8adbabaef9d28c52216
AMP26 <amit.patnaik0126@gmail.com>
2025-07-28 01:49:45 +0530

    Scalar Product

---
+++ 06-Matrices/matrices/src/scalerproduct/Main.java
@@ -0,0 +1,11 @@
+package scalerproduct;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int b = 2;
+
+        Solution sol = new Solution();
+        sol.scalarProduct(2, arr);
+    }
+}
---
+++ 06-Matrices/matrices/src/scalerproduct/Solution.java
@@ -0,0 +1,22 @@
+package scalerproduct;
+
+public class Solution {
+    public void scalarProduct(int b, int[][] mat) {
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[0].length; j++) {
+                mat[i][j] *= b;
+            }
+        }
+
+        printMatrix(mat);
+    }
+
+    private void printMatrix(int[][] mat) {
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0 ; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
