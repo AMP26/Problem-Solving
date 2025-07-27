16c03897dedd4220f54b83a7b47252290c994249
AMP26 <amit.patnaik0126@gmail.com>
2025-07-27 15:46:53 +0530

    Print Matrix

---
+++ 06-Matrices/matrices/src/printmatrix/Solution.java
@@ -0,0 +1,13 @@
+package printmatrix;
+
+public class Solution {
+
+    public void printMatrix(int[][] mat) {
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0 ; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
