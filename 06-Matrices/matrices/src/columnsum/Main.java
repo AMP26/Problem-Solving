471aa9923100bb1d86387962cd620d09604b0681
AMP26 <amit.patnaik0126@gmail.com>
2025-07-27 17:32:57 +0530

    Column Sum

---
+++ 06-Matrices/matrices/src/columnsum/Main.java
@@ -0,0 +1,14 @@
+package columnsum;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3,4}, {5,6,7,8}, {9,2,3,4}};
+
+        Solution sol = new Solution();
+        int[] result = sol.columnSum(arr);
+
+        for(int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
---
+++ 06-Matrices/matrices/src/columnsum/Solution.java
@@ -0,0 +1,18 @@
+package columnsum;
+
+public class Solution {
+    public int[] columnSum(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        int[] result = new int[m];
+
+        for(int j = 0; j < m; j++) {
+            int sum = 0;
+            for(int i = 0; i < n; i++) {
+                sum += mat[i][j];
+            }
+            result[j] = sum;
+        }
+
+        return result;
+    }
+}
