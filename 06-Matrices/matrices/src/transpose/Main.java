f6842dce8ea7b2352ea5fd1454c4bed1f105defa
AMP26 <amit.patnaik0126@gmail.com>
2025-07-28 23:51:20 +0530

    Matrix Transpose

---
+++ 06-Matrices/matrices/src/transpose/Main.java
@@ -0,0 +1,10 @@
+package transpose;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2},{1, 2}, {1, 2}};
+
+        Solution sol = new Solution();
+        sol.transpose(arr);
+    }
+}
---
+++ 06-Matrices/matrices/src/transpose/Solution.java
@@ -0,0 +1,23 @@
+package transpose;
+
+public class Solution {
+
+    public void transpose(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        int[][] result = new int[m][n];
+
+        for(int j = 0; j < m; j++) {
+            for(int i = 0; i < n; i++) { result[j][i] = mat[i][j]; }
+        }
+        printMatrix(result);
+    }
+
+    private void printMatrix(int[][] mat) {
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0 ; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
