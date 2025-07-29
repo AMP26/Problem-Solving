29c3975f3b6b0a6e1486154e21c5ec2523aa08d9
AMP26 <amit.patnaik0126@gmail.com>
2025-07-29 21:52:36 +0530

    Rotate Matrix

---
+++ 06-Matrices/matrices/src/rotatematrix/Main.java
@@ -0,0 +1,10 @@
+package rotatematrix;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] A  = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        Solution sol = new Solution();
+        sol.rotateMatrix(A);
+    }
+}
---
+++ 06-Matrices/matrices/src/rotatematrix/Solution.java
@@ -0,0 +1,33 @@
+package rotatematrix;
+
+public class Solution {
+    public void rotateMatrix(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < m; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        for(int i = 0 ; i < n; i++) {
+            for(int j = 0, x = m - 1; j < x; j++, x--) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][x];
+                mat[i][x] = temp;
+            }
+        }
+
+        printMatrix(mat);
+    }
+
+    private void printMatrix(int[][] mat) {
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0 ; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
