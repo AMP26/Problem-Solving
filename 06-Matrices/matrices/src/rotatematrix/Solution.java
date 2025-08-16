029ec6fffeda4b009b564d53e7fe81c07e2ed599
AMP26 <amit.patnaik0126@gmail.com>
2025-08-16 23:38:56 +0530

    Rotate Matrix

--- 06-Matrices/matrices/src/rotatematrix/Solution.java
+++ 06-Matrices/matrices/src/rotatematrix/Solution.java
@@ -25,7 +25,7 @@ public class Solution {
     private void printMatrix(int[][] mat) {
         for(int i = 0; i < mat.length; i++) {
             for(int j = 0 ; j < mat[0].length; j++) {
-                System.out.print(mat[i][j] + " ");
+                System.out.print(mat[i][j] + " "); 
             }
             System.out.println("");
         }
