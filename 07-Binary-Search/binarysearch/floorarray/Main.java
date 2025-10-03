e81b62c6a2bb13ee8b185525ce6073e79b24a098
AMP26 <amit.patnaik0126@gmail.com>
2025-10-03 12:28:22 +0530

    Floor in an Array

--- 07-Binary-Search/binarysearch/floorarray/Main.java
+++ 07-Binary-Search/binarysearch/floorarray/Main.java
@@ -3,7 +3,7 @@ package binarysearch.floorarray;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {2, 4, 8, 6, 10, 14, 16, 17, 18};
-        int target = 15;
+        int target = 12;
 
         FloorArray ca = new FloorArray();
         ca.binarySearch(nums, target);
