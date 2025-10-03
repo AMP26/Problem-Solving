1177a5645798323d7634654a7230ea59800b1ea1
AMP26 <amit.patnaik0126@gmail.com>
2025-10-03 12:26:16 +0530

    Floor in an Array

--- 07-Binary-Search/binarysearch/floorarray/FloorArray.java
+++ 07-Binary-Search/binarysearch/floorarray/FloorArray.java
@@ -7,5 +7,15 @@ public class FloorArray {
         System.out.println("Ceil Result: " + result1);
     }
 
-    
+    private int binarySearchResult(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+
+        while(start <= end) {
+            int mid = start + ( end - start) / 2;
+            if(nums[mid] == target) { return mid; }
+            else if(nums[mid] < target) { start = mid + 1; }
+            else { end = mid - 1; }
+        }
+        return end;
+    }
 }
